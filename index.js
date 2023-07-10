ae7b0de9af6dd176e66f4cca409d239e6a0e6de2
Christopher Dakin <christopherdakin1@gmail.com>
2023-07-10 03:12:47 -0400

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(join(__dirname, 'notes-app-client/build')));
 
